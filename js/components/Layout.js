ad960b93b87d1035e76d25e0309cb9878073b222
skripka <vitaliy.skripka23@gmail.com>
2017-02-17 17:49:59 +0200

    add styles

--- js/components/Layout.js
+++ js/components/Layout.js
@@ -5,6 +5,14 @@ import React from "react";
 
 export default class Layout extends React.Component {
     render () {
-        return <div>ты пидор</div>;
+        return <div id="wrapper">
+            <h1>Enter players name</h1>
+            <ul id="input-list">
+                <input class="form-control"/>
+                <input class="form-control"/>
+            </ul>
+            <button class="btn btn-success">Add player</button>
+            <button class="btn btn-danger">Remove player</button>
+        </div>;
     }
 }
