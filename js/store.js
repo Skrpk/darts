2372a40f0d1124bda0576af721861f02e6fe51d4
skripka <vitaliy.skripka23@gmail.com>
2017-02-20 12:12:04 +0200

    add store

---
+++ js/store.js
@@ -0,0 +1,11 @@
+/**
+ * Created by user on 20.02.2017.
+ */
+import reducer from "./reducers/reducer";
+import { applyMiddleware, createStore } from "redux";
+
+import logger from "redux-logger";
+
+const middleware = applyMiddleware(logger());
+
+export default createStore(reducer, middleware);
